f2e331c54ab99135d571979307747d6f31b5ec53
Le Hoang Than <lehoangthan584@gmail.com>
2026-04-18 23:54:38 +0700

    fix logic capitalize hackerrank

--- dsa/practice/hackerrank/strings/capitalize/solution.py
+++ dsa/practice/hackerrank/strings/capitalize/solution.py
@@ -1,14 +1,18 @@
 def solve(s):
-    words_list = s.split()
-    result = []
-    for word in words_list:
-        if str(word[0]).isalpha():
-            result.append(str(word[0]).upper() + word[1:])
-            print(result)
+    l = s
+    new_word = True
+    result= ''
+    for i in l:
+        if new_word and i.isalpha():
+            result= result+ i.upper()
+            new_word = False
+            continue
+        elif i == ' ':
+            new_word = True
         else:
-            result.append(word)
-    return " ".join(result)
-
+            new_word = False
+        result= result+ i
+    return result
 
 if __name__ == '__main__':
     print(solve(input()))
--- dsa/practice/hackerrank/strings/capitalize/test.py
+++ dsa/practice/hackerrank/strings/capitalize/test.py
@@ -1,4 +1,29 @@
 from solution import solve
 
-def test_solve():
-    assert solve('chris alan') == 'Chris Alan'
+
+def test_basic_words():
+    assert solve("hello world") == "Hello World"
+
+
+def test_mixed_word_number():
+    assert solve("1 w 2 r 3g") == "1 W 2 R 3g"
+
+
+def test_multiple_spaces():
+    assert solve("hello   world  lol") == "Hello   World  Lol"
+
+
+def test_leading_spaces():
+    assert solve("  hello world") == "  Hello World"
+
+
+def test_only_spaces():
+    assert solve("   ") == "   "
+
+
+def test_empty():
+    assert solve("") == ""
+
+
+def test_number_prefix():
+    assert solve("123abc test") == "123abc Test"
