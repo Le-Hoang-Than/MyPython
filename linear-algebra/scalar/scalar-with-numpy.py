4e6f7fa7657c7ac8407dcd8c8f5cc40c4043d05e
Le Hoang Than <lehoangthan584@gmail.com>
2026-05-16 21:58:43 +0700

    scalar

---
+++ linear-algebra/scalar/scalar-with-numpy.py
@@ -0,0 +1,5 @@
+import numpy as np
+
+a = np.array(20)
+b = np.array(200)
+print("diện tích x = 50 --> giá nhà y = a * 50 + b = ", a * 50 + b)
---
+++ linear-algebra/scalar/scalar-with-pytorch.py
@@ -0,0 +1,5 @@
+import torch
+
+a = torch.tensor(20)
+b = torch.tensor(200)
+print("diện tích x = 50 --> giá nhà y = a * 50 + b = ", a * 50 + b)
